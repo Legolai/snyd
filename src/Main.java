1477befebf4fc0178978e4690015bee6b679203a
Legolai <nico360f@gmail.com>
2021-09-18 18:05:24 +0200

    new clear

--- src/Main.java
+++ src/Main.java
@@ -7,7 +7,24 @@ public class Main {
     }
     public static void clearConsole()
     {
-        System.out.print("\033[H\033[2J");
-        System.out.flush();
+        try{
+            String operatingSystem = System.getProperty("os.name"); //Check the current operating system
+            if(operatingSystem.contains("Windows")){
+                ProcessBuilder pb = new ProcessBuilder("cmd", "/c", "cls");
+                Process startProcess = pb.inheritIO().start();
+
+                startProcess.waitFor();
+            } else {
+                ProcessBuilder pb = new ProcessBuilder("clear");
+                Process startProcess = pb.inheritIO().start();
+
+                startProcess.waitFor();
+            }
+        }catch(Exception e){
+            System.out.println(e);
+        }
+
+        //System.out.print("\033[H\033[2J");
+        //System.out.flush();
     }
 }
